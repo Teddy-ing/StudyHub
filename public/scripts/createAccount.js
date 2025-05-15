4cae965eaab215fe8264e2f1f13c2bf55cb49a1d
Dean S. <dean.a.serr@gmail.com>
2025-05-15 15:12:21 -0700

    Update HTML
    - "Login" and "Create Account" pages made
    - Updates for calendar event edits
    -Notes layout updated

---
+++ public/scripts/createAccount.js
@@ -0,0 +1,9 @@
+let accountForm = document.getElementById('create-account');
+
+accountForm.addEventListener('submit', () => {
+    let userName = accountForm.elements[0].value;
+    let userEmail = accountFrom.elements[1].value;
+    let password = accountForm.elements[2].value;
+    event.preventDefault();
+})
+
---
+++ public/scripts/login.js
@@ -0,0 +1,6 @@
+let loginForm = document.getElementById('login-form');
+
+loginForm.addEventListener('submit', () => {
+    let userEmail = loginForm.elements[0].value;
+    let password = loginForm.elemetns[1].value;
+})
